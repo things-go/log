eaa0d36b00245f76f20642bfaa40239e1fb0747e
thinkgos <thinkgo@aliyun.com>
2024-04-17 21:31:43 +0800

    fix: fix the code

--- logger.go
+++ logger.go
@@ -10,6 +10,10 @@ import (
 )
 
 // Log wrap zap logger
+// - methods named after the log level or ending in "Context" for log.Print-style logging
+// - methods ending in "w" or "wContext" for loosely-typed structured logging
+// - methods ending in "f" or "fContext" for log.Printf-style logging
+// - methods ending in "x" or "xContext" for structured logging
 type Log struct {
 	log   *zap.Logger
 	level zap.AtomicLevel
@@ -178,6 +182,308 @@ func (l *Log) Logx(ctx context.Context, level Level, msg string, fields ...Field
 	}
 }
 
+//****** named after the log level or ending in "Context" for log.Print-style logging
+
+// Debug (see DebugContext)
+func (l *Log) Debug(args ...any) {
+	l.DebugContext(l.ctx, args...)
+}
+
+// DebugContext uses fmt.Sprint to construct and log a message.
+func (l *Log) DebugContext(ctx context.Context, args ...any) {
+	l.Log(ctx, DebugLevel, args...)
+}
+
+// Info see InfoContext
+func (l *Log) Info(args ...any) {
+	l.InfoContext(l.ctx, args...)
+}
+
+// InfoContext uses fmt.Sprint to construct and log a message.
+func (l *Log) InfoContext(ctx context.Context, args ...any) {
+	l.Log(ctx, InfoLevel, args...)
+}
+
+// Warn see WarnContext
+func (l *Log) Warn(args ...any) {
+	l.WarnContext(l.ctx, args...)
+}
+
+// WarnContext uses fmt.Sprint to construct and log a message.
+func (l *Log) WarnContext(ctx context.Context, args ...any) {
+	l.Log(ctx, WarnLevel, args...)
+}
+
+// Error see ErrorContext
+func (l *Log) Error(args ...any) {
+	l.ErrorContext(l.ctx, args...)
+}
+
+// ErrorContext uses fmt.Sprint to construct and log a message.
+func (l *Log) ErrorContext(ctx context.Context, args ...any) {
+	l.Log(ctx, ErrorLevel, args...)
+}
+
+// DPanic see DPanicContext
+func (l *Log) DPanic(args ...any) {
+	l.DPanicContext(l.ctx, args...)
+}
+
+// DPanicContext uses fmt.Sprint to construct and log a message. In development, the
+// logger then panics. (see DPanicLevel for details.)
+func (l *Log) DPanicContext(ctx context.Context, args ...any) {
+	l.Log(ctx, DPanicLevel, args...)
+}
+
+// Panic see PanicContext
+func (l *Log) Panic(args ...any) {
+	l.PanicContext(l.ctx, args...)
+}
+
+// PanicContext uses fmt.Sprint to to construct and log a message, then panics.
+func (l *Log) PanicContext(ctx context.Context, args ...any) {
+	l.Log(ctx, PanicLevel, args...)
+}
+
+// Fatal see FatalContext
+func (l *Log) Fatal(args ...any) {
+	l.FatalContext(l.ctx, args...)
+}
+
+// FatalContext uses fmt.Sprint to construct and log a message, then calls os.Exit.
+func (l *Log) FatalContext(ctx context.Context, args ...any) {
+	l.Log(ctx, FatalLevel, args...)
+}
+
+//****** ending in "f" or "fContext" for log.Printf-style logging
+
+// Debugf see DebugfContext
+func (l *Log) Debugf(template string, args ...any) {
+	l.DebugfContext(l.ctx, template, args...)
+}
+
+// DebugfContext uses fmt.Sprintf to log a templated message.
+func (l *Log) DebugfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, DebugLevel, template, args...)
+}
+
+// Infof see InfofContext
+func (l *Log) Infof(template string, args ...any) {
+	l.InfofContext(l.ctx, template, args...)
+}
+
+// InfofContext uses fmt.Sprintf to log a templated message.
+func (l *Log) InfofContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, InfoLevel, template, args...)
+}
+
+// Warnf see WarnfContext
+func (l *Log) Warnf(template string, args ...any) {
+	l.WarnfContext(l.ctx, template, args...)
+}
+
+// WarnfContext uses fmt.Sprintf to log a templated message.
+func (l *Log) WarnfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, WarnLevel, template, args...)
+}
+
+// Errorf see ErrorfContext
+func (l *Log) Errorf(template string, args ...any) {
+	l.ErrorfContext(l.ctx, template, args...)
+}
+
+// ErrorfContext uses fmt.Sprintf to log a templated message.
+func (l *Log) ErrorfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, ErrorLevel, template, args...)
+}
+
+// DPanicf see DPanicfContext
+func (l *Log) DPanicf(template string, args ...any) {
+	l.DPanicfContext(l.ctx, template, args...)
+}
+
+// DPanicfContext uses fmt.Sprintf to log a templated message. In development, the
+// logger then panics. (see DPanicLevel for details.)
+func (l *Log) DPanicfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, DPanicLevel, template, args...)
+}
+
+// Panicf see PanicfContext
+func (l *Log) Panicf(template string, args ...any) {
+	l.PanicfContext(l.ctx, template, args...)
+}
+
+// PanicfContext uses fmt.Sprintf to log a templated message, then panics.
+func (l *Log) PanicfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, PanicLevel, template, args...)
+}
+
+// Fatalf see FatalfContext
+func (l *Log) Fatalf(template string, args ...any) {
+	l.FatalfContext(l.ctx, template, args...)
+}
+
+// Fatalf uses fmt.Sprintf to log a templated message, then calls os.Exit.
+func (l *Log) FatalfContext(ctx context.Context, template string, args ...any) {
+	l.Logf(ctx, FatalLevel, template, args...)
+}
+
+//****** ending in "w" or "wContext" for loosely-typed structured logging
+
+// Debugw see DebugwContext
+func (l *Log) Debugw(msg string, keysAndValues ...any) {
+	l.DebugwContext(l.ctx, msg, keysAndValues...)
+}
+
+// DebugwContext logs a message with some additional context. The variadic key-value or Field
+// pairs or Field are treated as they are in With.
+//
+// When debug-level logging is disabled, this is much faster than
+//
+//	s.With(fields).Debug(msg)
+func (l *Log) DebugwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, DebugLevel, msg, keysAndValues...)
+}
+
+// Infow see InfowContext
+func (l *Log) Infow(msg string, keysAndValues ...any) {
+	l.InfowContext(l.ctx, msg, keysAndValues...)
+}
+
+// InfowContext logs a message with some additional context. The variadic key-value
+// pairs or Field are treated as they are in With.
+func (l *Log) InfowContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, InfoLevel, msg, keysAndValues...)
+}
+
+// Warnw see WarnwContext
+func (l *Log) Warnw(msg string, keysAndValues ...any) {
+	l.WarnwContext(l.ctx, msg, keysAndValues...)
+}
+
+// WarnwContext logs a message with some additional context. The variadic key-value
+// pairs or Field are treated as they are in With.
+func (l *Log) WarnwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, WarnLevel, msg, keysAndValues...)
+}
+
+// Errorw see ErrorwContext
+func (l *Log) Errorw(msg string, keysAndValues ...any) {
+	l.ErrorwContext(l.ctx, msg, keysAndValues...)
+}
+
+// ErrorwContext logs a message with some additional context. The variadic key-value
+// pairs or Field are treated as they are in With.
+func (l *Log) ErrorwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, ErrorLevel, msg, keysAndValues...)
+}
+
+// DPanicw see DPanicwContext
+func (l *Log) DPanicw(msg string, keysAndValues ...any) {
+	l.DPanicwContext(l.ctx, msg, keysAndValues...)
+}
+
+// DPanicwContext logs a message with some additional context. In development, the
+// logger then panics. (see DPanicLevel for details.) The variadic key-value
+// pairs or Field are treated as they are in With.
+func (l *Log) DPanicwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, DPanicLevel, msg, keysAndValues...)
+}
+
+// Panicw see PanicwContext
+func (l *Log) Panicw(msg string, keysAndValues ...any) {
+	l.PanicwContext(l.ctx, msg, keysAndValues...)
+}
+
+// PanicwContext logs a message with some additional context, then panics. The
+// variadic key-value pairs or Field are treated as they are in With.
+func (l *Log) PanicwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, PanicLevel, msg, keysAndValues...)
+}
+
+func (l *Log) Fatalw(msg string, keysAndValues ...any) {
+	l.FatalwContext(l.ctx, msg, keysAndValues...)
+}
+
+// FatalwContext logs a message with some additional context, then calls os.Exit. The
+// variadic key-value pairs or Field are treated as they are in With.
+func (l *Log) FatalwContext(ctx context.Context, msg string, keysAndValues ...any) {
+	l.Logw(ctx, FatalLevel, msg, keysAndValues...)
+}
+
+//****** ending in "x" or "xContext" for structured logging
+
+// Debug (see DebugContext)
+func (l *Log) Debugx(msg string, fields ...Field) {
+	l.DebugxContext(l.ctx, msg, fields...)
+}
+
+// DebugContext uses fmt.Sprint to construct and log a message.
+func (l *Log) DebugxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, DebugLevel, msg, fields...)
+}
+
+// Info see InfoContext
+func (l *Log) Infox(msg string, fields ...Field) {
+	l.InfoxContext(l.ctx, msg, fields...)
+}
+
+// InfoContext uses fmt.Sprint to construct and log a message.
+func (l *Log) InfoxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, InfoLevel, msg, fields...)
+}
+
+// Warn see WarnContext
+func (l *Log) Warnx(msg string, fields ...Field) {
+	l.WarnxContext(l.ctx, msg, fields...)
+}
+
+// WarnContext uses fmt.Sprint to construct and log a message.
+func (l *Log) WarnxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, WarnLevel, msg, fields...)
+}
+
+// Error see ErrorContext
+func (l *Log) Errorx(msg string, fields ...Field) {
+	l.ErrorxContext(l.ctx, msg, fields...)
+}
+
+// ErrorContext uses fmt.Sprint to construct and log a message.
+func (l *Log) ErrorxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, ErrorLevel, msg, fields...)
+}
+
+// DPanic see DPanicContext
+func (l *Log) DPanicx(msg string, fields ...Field) {
+	l.DPanicxContext(l.ctx, msg, fields...)
+}
+
+// DPanicContext uses fmt.Sprint to construct and log a message. In development, the
+// logger then panics. (see DPanicLevel for details.)
+func (l *Log) DPanicxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, DPanicLevel, msg, fields...)
+}
+
+// Panic see PanicContext
+func (l *Log) Panicx(msg string, fields ...Field) {
+	l.PanicxContext(l.ctx, msg, fields...)
+}
+
+// PanicContext uses fmt.Sprint to to construct and log a message, then panics.
+func (l *Log) PanicxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, PanicLevel, msg, fields...)
+}
+
+// Fatal see FatalContext
+func (l *Log) Fatalx(msg string, fields ...Field) {
+	l.FatalxContext(l.ctx, msg, fields...)
+}
+
+// FatalContext uses fmt.Sprint to construct and log a message, then calls os.Exit.
+func (l *Log) FatalxContext(ctx context.Context, msg string, fields ...Field) {
+	l.Logx(ctx, FatalLevel, msg, fields...)
+}
+
 const (
 	_oddNumberErrMsg    = "Ignored key without a value."
 	_nonStringKeyErrMsg = "Ignored key-value pairs with non-string keys."
@@ -186,7 +492,7 @@ const (
 
 // formatMessage format with Sprint, Sprintf, or neither.
 // copy from zap(sugar.go)
-func formatMessage(template string, fmtArgs []interface{}) string {
+func formatMessage(template string, fmtArgs []any) string {
 	if len(fmtArgs) == 0 {
 		return template
 	}
@@ -201,14 +507,12 @@ func formatMessage(template string, fmtArgs []interface{}) string {
 	return fmt.Sprint(fmtArgs...)
 }
 
-func (l *Log) appendSweetenFields(fields []Field, args []interface{}) []Field {
+func (l *Log) appendSweetenFields(fields []Field, args []any) []Field {
 	if len(args) == 0 {
 		return nil
 	}
 
 	var (
-		// Allocate enough space for the worst case; if users pass only structured
-		// fields, we shouldn't penalize them with extra allocations.
 		invalid   invalidPairs
 		seenError bool
 	)
@@ -263,7 +567,7 @@ func (l *Log) appendSweetenFields(fields []Field, args []interface{}) []Field {
 
 type invalidPair struct {
 	position   int
-	key, value interface{}
+	key, value any
 }
 
 func (p invalidPair) MarshalLogObject(enc zapcore.ObjectEncoder) error {
